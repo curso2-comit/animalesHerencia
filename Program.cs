3bd127c81fe44abd29a71cc03284f9404ba9e495
jlalla <juanlalla@gmail.com>
2020-10-23 20:21:57 -0300

    Subo proyecto realizado en clase de POO con C#

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AnimalesHerencia
+{
+    public abstract class Animal { 
+        public abstract void Saludar();         
+    } 
+
+    public class Perro : Animal 
+    { 
+        public override void Saludar() 
+        {
+            Console.WriteLine("GUAU! Soy un perro!"); 
+        } 
+    }
+
+    public class Gato : Animal 
+    { 
+        public override void Saludar() 
+        {
+            Console.WriteLine("MIAU! No me jodas!"); 
+        } 
+    }
+
+    public class Tortuga : Animal 
+    { 
+        public override void Saludar() 
+        {
+            //al ser Saludar un método abstracto, obligo que se implemente
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Animal animal = new Animal();  //no es posible crear objetos abstractos
+            // animal.Saludar(); 
+            Perro dog = new Perro(); 
+            dog.Saludar();
+            Gato gato = new Gato();
+            gato.Saludar();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AnimalesHerencia
+Aplicación de Consola con prácticas durante la explicación de Programación orientada a Objetos en C#
